91e046a0ac23e3d2eb3e93d253c3a1f7d95a42cd
Michael Moorman <michael.l.moorman@protonmail.com>
2020-05-19 13:05:32 -0500

    Fleshing out stack demo slightly

--- stackdemo.s
+++ stackdemo.s
@@ -3,4 +3,34 @@
 	!to "stackdemo", cbm
 	* = $1300
 
-start:	
+level1:
+	lda #$aa
+	pha
+	nop
+	jsr level2
+	nop
+	pla
+	rts
+	nop
+	nop
+level2:
+	lda #$bb
+	pha
+	nop
+	jsr level3
+	nop
+	pla
+	rts
+	nop
+	nop
+level3:
+	lda #$cc
+	pha
+	nop
+	jsr level3
+	nop
+	pla
+	rts
+	nop
+	nop
+	
